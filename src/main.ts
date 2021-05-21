3f183ca69d2038d00d55e0b67f08604471c09a18
UnRunDead <me@unrundead.dev>
2021-05-21 22:47:36 +0200

    Created project and added .env handling

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+  ConfigModule.forRoot();
+  logger.log('Load .env file');
+
+  const port = Number(process.env.PORT);
+  logger.log(`API running on port: ${port}`);
+  await app.listen(port);
+}
+
+bootstrap();
