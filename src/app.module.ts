42cbd857fea41de897f0aac1a909699ee4e3eb3d
UnRunDead <me@unrundead.dev>
2021-05-23 15:01:21 +0200

    Added AuthModule to App

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { GameFilesModule } from './game-files/game-files.module';
 import { EntryListModule } from './entry-list/entry-list.module';
 import { ResultsModule } from './results/results.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [GameFilesModule, EntryListModule, ResultsModule],
+  imports: [GameFilesModule, EntryListModule, ResultsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
