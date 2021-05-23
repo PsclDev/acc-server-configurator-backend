b2c43a5ac7631304c16be1112b338077a88e9b6d
UnRunDead <me@unrundead.dev>
2021-05-23 14:59:00 +0200

    Added jwt

---
+++ src/auth/jwt-strategy.ts
@@ -0,0 +1,25 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+import { AuthorizationRepository } from './auth.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authRepo: AuthorizationRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.authRepo.findUser(username);
+
+    if (!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}
