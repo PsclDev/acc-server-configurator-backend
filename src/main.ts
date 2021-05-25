3ac1e9a8ae0444f33e8b94bf56c5d87eac358c86
UnRunDead <me@unrundead.dev>
2021-05-25 12:57:07 +0200

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const logger = new Logger('Main');
   ConfigModule.forRoot({ isGlobal: true });
   logger.log('Load .env file');
