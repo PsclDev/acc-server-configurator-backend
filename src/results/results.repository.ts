bb6d75eedc1d4e13a3b52412a3eba6655bdb7090
UnRunDead <me@unrundead.dev>
2021-06-03 21:26:28 +0200

    found bug in acc result files, edited repository to fix this issue

--- src/results/results.repository.ts
+++ src/results/results.repository.ts
@@ -10,10 +10,27 @@ export class ResultsRepository {
     for (const fileIdx in files) {
       const fileName = files[fileIdx];
       const fileData = readFileSync(`${dirPath}/${fileName}`, 'utf-8');
-      const json = JSON.parse(fileData);
+
+      const cleanedJson = await this.cleanJsonFile(fileName, fileData);
+      console.log(cleanedJson);
+
+      const json = JSON.parse(cleanedJson);
       results.push({ name: fileName, data: json });
     }
 
+    console.log(results);
     return results;
+  }
+
+  private async cleanJsonFile(
+    session: string,
+    content: string,
+  ): Promise<string> {
+    if (session.includes('FP'))
+      return content.replace('{\n    "sessionType": "FP",', '{');
+    if (session.includes('Q'))
+      return content.replace('{\n    "sessionType": "Q",', '{');
+    if (session.includes('R'))
+      return content.replace('{\n    "sessionType": "R",', '{');
   }
 }
