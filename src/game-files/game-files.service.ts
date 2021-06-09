e631770284e2ce65b1d86c23d16bb5cba6845776
UnRunDead <me@unrundead.dev>
2021-06-09 21:58:41 +0200

    Changes for sesssion return - needs refactor

--- src/game-files/game-files.service.ts
+++ src/game-files/game-files.service.ts
@@ -6,6 +6,7 @@ import { event } from '../../data/game-files/event.json';
 import { eventRules } from '../../data/game-files/eventRules.json';
 import { settings } from '../../data/game-files/settings.json';
 import { GameFilesRepository } from './game-files.repository';
+import { ParameterPermission } from './parameter/parameter-permission.enum';
 
 @Injectable()
 export class GameFilesService {
@@ -42,10 +43,63 @@ export class GameFilesService {
   }
 
   async getEvent(): Promise<ParameterDto[]> {
-    return await this.gameFilesRepo.getFileContentWithValues(
+    const list = await this.gameFilesRepo.getFileContentWithValues(
       event,
       `${this.baseServerSettigsPath}/event.json`,
     );
+
+    return await this.correctSessionArrayToSingleValues(list);
+  }
+
+  private async correctSessionArrayToSingleValues(list) {
+    const sessionsIndex = list.findIndex((entry) => entry.name === 'sessions');
+    const sessions = list[sessionsIndex];
+    list.splice(sessionsIndex, 1);
+    const sessionsValue = JSON.parse(sessions.value);
+
+    //TODO REFACTOR WITH NEW FILE AND CREATE FAKE "SETTING"
+    for (const idx in sessionsValue) {
+      const config = sessionsValue[idx];
+      const keys = Object.keys(config);
+      const keyIdxSessionType = keys.findIndex(
+        (entry) => entry === 'sessionType',
+      );
+
+      const values = Object.values(config);
+      const sessionName = await this.getNameFromSessionType(
+        values[keyIdxSessionType],
+      );
+
+      keys.splice(keyIdxSessionType);
+      values.splice(keyIdxSessionType);
+
+      for (const innerIdx in keys) {
+        const param: ParameterDto = {
+          name: `${sessionName} - ${keys[innerIdx]}`,
+          description: '',
+          value: values[innerIdx],
+          value_type: 'number',
+          required_permissions: ParameterPermission.USER,
+        };
+
+        list.push(param);
+      }
+    }
+
+    return list;
+  }
+
+  private async getNameFromSessionType(sessionType): Promise<string> {
+    switch (sessionType) {
+      case 'P':
+        return 'Practice';
+
+      case 'Q':
+        return 'Qualifying';
+
+      case 'R':
+        return 'Race';
+    }
   }
 
   async patchEvent(parameters: ParameterDto[]): Promise<string> {
