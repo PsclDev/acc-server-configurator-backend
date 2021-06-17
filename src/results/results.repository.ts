f848bc2bc95922784c0bec851ec93b05ad19f27b
UnRunDead <me@unrundead.dev>
2021-06-17 21:42:42 +0200

    bug fix in file read because of encoding

--- src/results/results.repository.ts
+++ src/results/results.repository.ts
@@ -6,29 +6,22 @@ export class ResultsRepository {
   async getResults(dirPath: string): Promise<any[]> {
     const results = [];
     const files = readdirSync(dirPath);
-
-    for (const fileIdx in files) {
-      const fileName = files[fileIdx];
-      const fileData = readFileSync(`${dirPath}/${fileName}`, 'utf-8');
-
-      const cleanedJson = await this.correctJsonFile(fileName, fileData);
-      const json = JSON.parse(cleanedJson);
-
-      results.push({ name: fileName, data: json });
-    }
+    const fileName = files[0];
+    const filePath = `${dirPath}/${fileName}`;
+    const fileData = readFileSync(filePath, 'utf-8');
+    const cleanedJson = await this.correctJsonFile(fileData);
+    const json = JSON.parse(cleanedJson);
+    results.push({ name: fileName, data: json });
 
     return results;
   }
 
-  private async correctJsonFile(
-    session: string,
-    content: string,
-  ): Promise<string> {
-    if (session.includes('FP'))
-      return content.replace('{\n    "sessionType": "FP",', '{');
-    if (session.includes('Q'))
-      return content.replace('{\n    "sessionType": "Q",', '{');
-    if (session.includes('R'))
-      return content.replace('{\n    "sessionType": "R",', '{');
+  private async correctJsonFile(fileData: string): Promise<string> {
+    if (fileData.includes('FP'))
+      return fileData.replace('{\n    "sessionType": "FP",', '{');
+    if (fileData.includes('Q'))
+      return fileData.replace('{\n    "sessionType": "Q",', '{');
+    if (fileData.includes('R'))
+      return fileData.replace('{\n    "sessionType": "R",', '{');
   }
 }
