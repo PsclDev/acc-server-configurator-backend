2f056ffa933f5f696ead967f2cacaac014ada089
UnRunDead <me@unrundead.dev>
2021-06-18 21:56:06 +0200

    finished first version of results array with all data

--- src/results/results.service.ts
+++ src/results/results.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ResultsRepository } from './results.repository';
+import { carList } from 'data/game-files/carModelList.json';
 
 @Injectable()
 export class ResultsService {
@@ -10,90 +11,46 @@ export class ResultsService {
   async getResults(): Promise<any[]> {
     const results = [];
     const resultsData = await this.resultsRepo.getResults(this.dirPath);
-
     for (const idx in resultsData) {
       const unformattedResult = resultsData[idx];
       const unformattedData = unformattedResult.data;
+      const unformattedSessionResult = unformattedData.sessionResult;
+      const unformattedLaps = unformattedData.laps;
+      const unformattedPenalties = unformattedData.penalties.concat(
+        unformattedData.post_race_penalties,
+      );
 
       const result = {
         name: await this.getTitle(
           unformattedResult.name,
           unformattedData.trackName,
         ),
-        isWeatherWet: unformattedData.sessionResult.isWetSession,
+        isWeatherWet: unformattedSessionResult.isWetSession,
         bestLap: await this.getTimeValueAsString(
-          String(unformattedData.sessionResult.bestlap),
+          String(unformattedSessionResult.bestlap),
         ),
         bestSplits: {
           sectorOne: await this.getTimeValueAsString(
-            String(unformattedData.sessionResult.bestSplits[0]),
+            String(unformattedSessionResult.bestSplits[0]),
           ),
           sectorTwo: await this.getTimeValueAsString(
-            String(unformattedData.sessionResult.bestSplits[1]),
+            String(unformattedSessionResult.bestSplits[1]),
           ),
           sectorThree: await this.getTimeValueAsString(
-            String(unformattedData.sessionResult.bestSplits[2]),
+            String(unformattedSessionResult.bestSplits[2]),
           ),
         },
-        cars: [
-          {
-            car: {
-              raceNumber: 0,
-              name: 'Porsche',
-              teamName: '',
-              bestLap: 111948,
-              bestSplits: {
-                sectorOne: 35565,
-                sectorTwo: 39957,
-                sectorThree: 36426,
-              },
-              totalTime: 407894,
-              lapCount: 3,
-              missingMandatoryPitstop: -1,
-              drivers: [
-                {
-                  driver: {
-                    name: 'Max Mustermann',
-                    shortname: 'MM',
-                  },
-                },
-              ],
-              laps: [
-                {
-                  lap: {
-                    driver: 'Max Mustermann',
-                    laptime: 109446,
-                    isValidForBest: false,
-                    splits: {
-                      sectorOne: 35565,
-                      sectorTwo: 39957,
-                      sectorThree: 36426,
-                    },
-                  },
-                },
-              ],
-              penalties: [
-                {
-                  penaltie: {
-                    driver: 'Max Mustermann',
-                    reason: 'Cutting',
-                    penalty: 'Drive Through',
-                    violationInLap: 7,
-                    clearedInLap: 9,
-                  },
-                },
-              ],
-            },
-          },
-        ],
+        cars: await this.getCarsArray(
+          unformattedSessionResult.leaderBoardLines,
+          unformattedLaps,
+          unformattedPenalties,
+        ),
       };
 
-      console.log(result);
       results.push(result);
     }
 
-    return [];
-    //return results;
+    return results;
   }
 
   private async getTitle(filename: string, trackName: string): Promise<string> {
@@ -130,27 +87,8 @@ export class ResultsService {
     return arr.join(' ');
   }
 
-  private async formatData(json: any): Promise<any> {
-    json.sessionResult.bestlap = await this.getTimeValueAsString(
-      String(json.sessionResult.bestlap),
-    );
-
-    json.sessionResult.bestSplits[0] = await this.getTimeValueAsString(
-      String(json.sessionResult.bestSplits[0]),
-    );
-
-    json.sessionResult.bestSplits[1] = await this.getTimeValueAsString(
-      String(json.sessionResult.bestSplits[1]),
-    );
-
-    json.sessionResult.bestSplits[2] = await this.getTimeValueAsString(
-      String(json.sessionResult.bestSplits[2]),
-    );
-
-    return json;
-  }
-
   private async getTimeValueAsString(value: string): Promise<string> {
+    value = value.toString();
     const ms = value.substring(value.length - 3, value.length);
     let time = value.replace(ms, '');
 
@@ -176,5 +114,110 @@ export class ResultsService {
     }
 
     return formattedString;
+  }
+
+  private async getCarsArray(
+    sessionResults: any,
+    laps: any,
+    penalties: any,
+  ): Promise<any[]> {
+    const carArr = [];
+
+    for (const carIdx in sessionResults) {
+      const carObj = sessionResults[carIdx];
+      const carDetails = carObj.car;
+      const carTiming = carObj.timing;
+      const carDrivers = carDetails.drivers;
+
+      const car = {
+        id: carDetails.carId,
+        raceNumber: carDetails.raceNumber,
+        name: await this.getCarNameByIndex(carDetails.carModel),
+        teamName: carDetails.teamName,
+        bestLap: await this.getTimeValueAsString(carTiming.bestLap),
+        bestSplits: {
+          sectorOne: await this.getTimeValueAsString(carTiming.bestSplits[0]),
+          sectorTwo: await this.getTimeValueAsString(carTiming.bestSplits[1]),
+          sectorThree: await this.getTimeValueAsString(carTiming.bestSplits[2]),
+        },
+        totalTime: await this.getTimeValueAsString(carTiming.totalTime),
+        lapCount: carTiming.lapCount,
+        missingMandatoryPitstop: carObj.missingMandatoryPitstop,
+        drivers: await this.getDriversArray(carDrivers),
+        laps: await this.getLapsArray(laps),
+        penalties: await this.getPenaltyArray(penalties),
+      };
+
+      carArr.push(car);
+    }
+
+    return carArr;
+  }
+
+  private async getCarNameByIndex(index: number): Promise<string> {
+    const car = carList.filter((car) => car.value === index);
+    return car[0].name;
+  }
+
+  private async getDriversArray(drivers: any): Promise<any[]> {
+    const driverArr = [];
+    let itemIndex = 0;
+
+    for (const driverIdx in drivers) {
+      const driverJson = drivers[driverIdx];
+      const driver = {
+        id: itemIndex,
+        name: `${driverJson.firstName} ${driverJson.lastName}`,
+        shortName: `${driverJson.shortName}`,
+      };
+
+      driverArr.push(driver);
+      itemIndex++;
+    }
+
+    return driverArr;
+  }
+
+  private async getLapsArray(laps: any): Promise<any[]> {
+    const lapsArr = [];
+
+    for (const lapIdx in laps) {
+      const lapJson = laps[lapIdx];
+
+      const lap = {
+        driverIndex: lapJson.driverIndex,
+        isValidForBest: lapJson.isValidForBest,
+        laptime: await this.getTimeValueAsString(lapJson.laptime),
+        splits: {
+          sectorOne: await this.getTimeValueAsString(lapJson.splits[0]),
+          sectorTwo: await this.getTimeValueAsString(lapJson.splits[1]),
+          sectorThree: await this.getTimeValueAsString(lapJson.splits[2]),
+        },
+      };
+
+      lapsArr.push(lap);
+    }
+
+    return lapsArr;
+  }
+
+  private async getPenaltyArray(penalties: any): Promise<any[]> {
+    const penaltyArr = [];
+
+    for (const penaltyIdx in penalties) {
+      const penaltyJson = penalties[penaltyIdx];
+
+      const penalty = {
+        driverIndex: penaltyJson.driverIndex,
+        reason: penaltyJson.reason,
+        penalty: penaltyJson.penalty,
+        violationInLap: penaltyJson.violationInLap,
+        clearedInLap: penaltyJson.clearedInLap,
+      };
+
+      penaltyArr.push(penalty);
+    }
+
+    return penaltyArr;
   }
 }
