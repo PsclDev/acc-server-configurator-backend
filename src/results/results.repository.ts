f4731f873282b7fb6223986362e39d6fef777aed
UnRunDead <me@unrundead.dev>
2021-06-18 21:55:46 +0200

    removed single index use, switched back to all files in array

--- src/results/results.repository.ts
+++ src/results/results.repository.ts
@@ -6,12 +6,14 @@ export class ResultsRepository {
   async getResults(dirPath: string): Promise<any[]> {
     const results = [];
     const files = readdirSync(dirPath);
-    const fileName = files[0];
-    const filePath = `${dirPath}/${fileName}`;
-    const fileData = readFileSync(filePath, 'utf-8');
-    const cleanedJson = await this.correctJsonFile(fileData);
-    const json = JSON.parse(cleanedJson);
-    results.push({ name: fileName, data: json });
+    for (const idx in files) {
+      const fileName = files[idx];
+      const filePath = `${dirPath}/${fileName}`;
+      const fileData = readFileSync(filePath, 'utf-8');
+      const cleanedJson = await this.correctJsonFile(fileData);
+      const json = JSON.parse(cleanedJson);
+      results.push({ name: fileName, data: json });
+    }
 
     return results;
   }
