fbef405a3ce863d3ff8c552b7e95efce70d10734
UnRunDead <me@pscl.dev>
2021-07-27 20:06:56 +0200

    added options to use https and cors via .env file

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigModule } from '@nestjs/config';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-
   const logger = new Logger('Main');
   ConfigModule.forRoot({ isGlobal: true });
   logger.log('Load .env file');
+
+  const https = Boolean(process.env.USE_HTTPS);
+  let app: INestApplication = null;
+
+  if (https) {
+    logger.log('Using https');
+    const key = process.env.HTTPS_KEY;
+    const cert = process.env.HTTPS_CERT;
+
+    const httpsOptions = {
+      key: fs.readFileSync(key),
+      cert: fs.readFileSync(cert),
+    };
+
+    app = await NestFactory.create(AppModule, { httpsOptions });
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
+  const useCors = Boolean(process.env.USE_CORS);
+  if (useCors) {
+    app.enableCors();
+    logger.log('Enabled Cors');
+  }
 
   const port = Number(process.env.PORT);
   logger.log(`API running on port: ${port}`);
